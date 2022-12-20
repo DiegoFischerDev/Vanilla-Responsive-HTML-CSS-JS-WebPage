b76a175d1e4aa6d6701d94d76d36cb74dcad9fcd
DiegoFischerDev <diegofischer.dev@gmail.com>
2022-12-20 17:13:54 -0300

    finished menu bar

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// /****** section_one  ****************/
+// const section_one = document.querySelector('#section_one')
+// const section_menu = document.querySelector('#section_menu')
+
+// function hiden_section_one() {
+//   section_one.classList.toggle('hiden')
+//   section_menu.classList.toggle('hiden')
+// }
+
+
+/******* Section_four **************/
 $(".option").click(function () {
   $(".option").removeClass("active");
   $(this).addClass("active");
